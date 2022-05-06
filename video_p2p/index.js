bf788bee0d5e23fad4fd4442f0b7b40c88d4cee7
nicolas <nicolas@nicolass-MacBook-Pro.local>
2022-05-06 13:06:49 +0200

    Add test

--- video_p2p/index.js
+++ video_p2p/index.js
@@ -1,4 +1,33 @@
+function gotMedia (stream) {
+  var peer1 = new SimplePeer({ initiator: true, stream: stream })
+  var peer2 = new SimplePeer()
 
+  peer1.on('signal', data => {
+    peer2.signal(data)
+  })
+
+  peer2.on('signal', data => {
+    peer1.signal(data)
+  })
+
+  peer2.on('stream', stream => {
+    // got remote video stream, now let's show it in a video tag
+    var video = document.querySelector('video')
+
+    if ('srcObject' in video) {
+      video.srcObject = stream
+    } else {
+      video.src = window.URL.createObjectURL(stream) // for older browsers
+    }
+
+    video.play()
+  })
+}
+// get video/voice stream
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: false
+}).then(gotMedia).catch(() => {})
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: false }, function (err, stream) {
   if (err) return console.error(err)
