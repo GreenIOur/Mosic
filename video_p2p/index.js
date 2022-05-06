c68bf0fd1f38edecb9de8238f22e3dab24f1dec3
nicolas <nicolas@nicolass-MacBook-Pro.local>
2022-05-06 12:51:49 +0200

    video_p2p add

---
+++ video_p2p/index.js
@@ -0,0 +1,37 @@
+
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: false }, function (err, stream) {
+  if (err) return console.error(err)
+
+  var peer = new SimplePeer({
+    initiator: location.hash === '#init',
+    trickle: false,
+    stream: stream
+  })
+
+  peer.on('signal', function (data) {
+    document.getElementById('yourId').value = JSON.stringify(data)
+  })
+
+  document.getElementById('connect').addEventListener('click', function () {
+    var otherId = JSON.parse(document.getElementById('otherId').value)
+    peer.signal(otherId)
+  })
+
+  document.getElementById('send').addEventListener('click', function () {
+    var yourMessage = document.getElementById('yourMessage').value
+    peer.send(yourMessage)
+  })
+
+  peer.on('data', function (data) {
+    document.getElementById('messages').textContent += data + '\n'
+  })
+
+  peer.on('stream', function (stream) {
+    var video = document.createElement('video')
+    document.body.appendChild(video)
+
+    video.src = window.URL.createObjectURL(stream)
+    video.play()
+  })
+})
